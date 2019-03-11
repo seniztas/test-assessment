3b2a9d8bd06b1494862852aa04c3085614aaab5a
Seniz Tascioglu <seniz.tascioglu@outlook.com>
2019-03-11 16:12:07 +0100

    upt case on movie

--- test-assessment-master/iamdb/test/test_movie.py
+++ test-assessment-master/iamdb/test/test_movie.py
@@ -1,4 +1,4 @@
-from testhelpers import post_api, get_api, delete_api
+from testhelpers import post_api, get_api, delete_api, put_api
 
 class TestMovie(object):
     #test cases for POST#
@@ -39,7 +39,7 @@ class TestMovie(object):
 
     def test_get_movie_with_invalid_id(self):
         status_code, data = get_api('movies', '6652f161-d664-4a3c-aaf8-99ff0880c381')
-        assert status_code == 404, 'No persons found'
+        assert status_code == 404
     
     def test_get_movie_with_null_id(self):
         status_code, data = get_api('movies', '')
@@ -48,4 +48,15 @@ class TestMovie(object):
      #test cases for DELETE#
     def test_delete_movie_with_invalid_id(self):
         status_code, data = delete_api('movies', 'b9f2d28c-57dc-4381-a752-ac36611ab51b')
-        assert status_code == 404, 'No persons found'
+        assert status_code == 404
+
+    #test cases for PUT#
+    def test_put_movie_with_invalid_id(self):
+        update_exist_movie = {'title': 'Captan Marvel', 'year': 2019}
+        status_code, data = put_api('movies', 'e6068a8f-fb38-49b5-a1a9-2e1222a2bdce',update_exist_movie)
+        assert status_code == 200
+        assert data['title'] == 'Captan Marvel'
+        assert data['year'] == 2019
+        return_movie_data = {"title": "Die Hard", "year": 1988}
+        put_api('movies', 'e6068a8f-fb38-49b5-a1a9-2e1222a2bdce',return_movie_data)
+
